0da90bb6c2f9d58a870eb2b8418f86fadf0559a6
zak39 <fotia.baptiste@hotmai.com>
2024-09-28 22:19:56 +0200

    feature(controller,twig): Created fake hard data and twig formatting

--- src/Controller/AnnuaireController.php
+++ src/Controller/AnnuaireController.php
@@ -11,8 +11,26 @@ class AnnuaireController extends AbstractController
     #[Route('/', name: 'app_annuaire')]
     public function index(): Response
     {
+        $contacts = [
+            [
+                'nom' => 'Dupont',
+                'prenom' => 'Jean',
+                'telephone' => '0123456789'
+            ],
+            [
+                'nom' => 'Durant',
+                'prenom' => 'Marie',
+                'telephone' => '0123456789'
+            ],
+            [
+                'nom' => 'Bouhadana',
+                'prenom' => 'Mohamed',
+                'telephone' => '0123456789'
+            ],
+        ];
+
         return $this->render('annuaire/index.html.twig', [
-            'controller_name' => 'AnnuaireController',
+            'contacts' => $contacts,
         ]);
     }
 }
