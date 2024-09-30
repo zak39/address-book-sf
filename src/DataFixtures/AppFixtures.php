452c256d3e46173f3e3863a7ddf3f107b1865d98
zak39 <fotia.baptiste@hotmail.com>
2024-09-30 23:30:26 +0200

    feature(fixutres): Create first fixtures for contacts
    We need to run "symfony console doctrine:fixtures:load" to apply
    fixtures.

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Contact;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $contacts = [
+            $this->createContact('Dupont', 'Jean', '0123456789'),
+            $this->createContact('Durant', 'Marie', '0123456789'),
+            $this->createContact('Bouhadana', 'Mohamed', '0123456789'),
+        ];
+
+        foreach ($contacts as $contact) {
+            $manager->persist($contact);
+        }
+
+        $manager->flush();
+    }
+
+    private function createContact(string $nom, string $prenom, string $telephone): Contact {
+        $contact = new Contact();
+        $contact
+            ->setNom($nom)
+            ->setPrenom($prenom)
+            ->setTelephone($telephone)
+        ;
+
+        return $contact;
+    }
+}
